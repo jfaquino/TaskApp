b13e84c83c9da0eb52ef1227ab79b8eb25d5ad79
Jose Felipe J. Aquino <gecafe1@gmail.com>
2018-06-09 11:51:06 -0400

    Creacion Layout Categoria

--- app/src/main/java/ado/edu/itla/taskapp/MainActivity.java
+++ app/src/main/java/ado/edu/itla/taskapp/MainActivity.java
@@ -1,13 +1,37 @@
 package ado.edu.itla.taskapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import ado.edu.itla.taskapp.entidad.Categoria;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCategoria = findViewById(R.id.btnCategoria);
+        btnCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CategoriaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Categoria cat = new Categoria();
+
+        cat.setDescripcion("CATEGORIA 1");
+        cat.setId(2);
+        Log.i(LOG_TAG, cat.toString());
+
     }
 }
