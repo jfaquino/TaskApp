0bae83f8c59cf9d1aaf0c40d26fcba32e19f8fe9
Jose Felipe J. Aquino <gecafe1@gmail.com>
2018-06-16 10:48:59 -0400

    Creacion de base de datos

--- app/src/main/java/ado/edu/itla/taskapp/CategoriaActivity.java
+++ app/src/main/java/ado/edu/itla/taskapp/CategoriaActivity.java
@@ -2,12 +2,42 @@ package ado.edu.itla.taskapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import ado.edu.itla.taskapp.entidad.Categoria;
+import ado.edu.itla.taskapp.repositorio.CategoriaRepositorio;
+import ado.edu.itla.taskapp.repositorio.db.CategoriaRepositorioDbImp;
 
 public class CategoriaActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "CategoriaActivity";
+    private CategoriaRepositorio categoriaRepositorio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categoria);
+
+        categoriaRepositorio =  new CategoriaRepositorioDbImp(this);
+
+        final EditText txtNombre =  (EditText) findViewById(R.id.txtNombreCategoria);
+        Button btnGuardar = (Button) findViewById(R.id.btnGuardarCategoria);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Categoria categoria = new Categoria();
+                categoria.setDescripcion(txtNombre.getText().toString());
+
+                Log.i(LOG_TAG,categoria.toString());
+
+                categoriaRepositorio.guardar(categoria);
+                Log.i(LOG_TAG, categoria.toString());
+            }
+        });
     }
 }
---
+++ app/src/main/java/ado/edu/itla/taskapp/repositorio/db/CategoriaRepositorioDbImp.java
@@ -0,0 +1,61 @@
+package ado.edu.itla.taskapp.repositorio.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
+import ado.edu.itla.taskapp.entidad.Categoria;
+import ado.edu.itla.taskapp.repositorio.CategoriaRepositorio;
+
+/**
+ * Created by MESCyT on 16/6/2018.
+ */
+
+public class CategoriaRepositorioDbImp implements CategoriaRepositorio {
+
+    private  ConexionDb conexionDb;
+    private static final String CAMPO_NOMBRE = "nombre";
+    private static final String TABLA_CATEGORIA = "categoria";
+
+    public CategoriaRepositorioDbImp(Context context) {
+
+        conexionDb = new ConexionDb(context);
+    }
+
+    @Override
+    public boolean guardar(Categoria categoria) {
+
+        ContentValues cv = new ContentValues();
+        cv.put(CAMPO_NOMBRE, categoria.getDescripcion());
+
+        SQLiteDatabase db = conexionDb.getWritableDatabase();
+        Long id = db.insert(TABLA_CATEGORIA,null,cv);
+
+        if (id.intValue() > 0){
+            categoria.setId(id.intValue());
+            return true;
+        }else {
+              return false;
+
+        }
+
+    }
+
+    @Override
+
+    public boolean actualizar(Categoria categoria) {
+        return false;
+    }
+
+    @Override
+    public Categoria buscar(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Categoria> buscar(String nombre) {
+        return null;
+    }
+}
