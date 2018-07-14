9c939a557d78ed6f8da3882aa6ecc75fc6f19455
Felipe Aquino <gecafe1@gmail.com>
2018-07-13 23:14:30 -0400

    Creación de los activity LogIn y Registro Usuarios

---
+++ app/src/main/java/ado/edu/itla/taskapp/vista/LogInActivity.java
@@ -0,0 +1,15 @@
+package ado.edu.itla.taskapp.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "LogInActivity";
+
+    
+}
---
+++ app/src/main/java/ado/edu/itla/taskapp/vista/RegistroActivity.java
@@ -0,0 +1,6 @@
+package ado.edu.itla.taskapp.vista;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class RegistroActivity extends AppCompatActivity {
+}
